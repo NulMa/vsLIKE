0203c3aa2913fe6043f3f318196ded5a39226047
NullMa <86356074+NulMa@users.noreply.github.com>
2023-03-08 16:01:12 +0900

    prac_5

---
+++ Assets/scripts/Enemy.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D target;
+
+    bool isLive = true;
+    
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+
+
+    private void Awake() {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate(){
+
+        if (!isLive)
+            return;
+
+        Vector2 dirVec = target.position - rigid.position;
+        Vector2 nextVec = dirVec.normalized * speed * Time.deltaTime; //프레임 영향 평준화 주의
+        rigid.MovePosition(rigid.position + nextVec);
+        rigid.velocity = Vector2.zero; //물리적 속도가 이동에 영향을 주지 않게 하기 위함
+
+    }
+
+    private void LateUpdate() {
+
+        if (!isLive)
+            return;
+
+        spriter.flipX = target.position.x < rigid.position.x;
+    }
+}
--- Assets/scripts/Reposition.cs
+++ Assets/scripts/Reposition.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Reposition : MonoBehaviour{
 
+
+    Collider2D coll;
+
+    private void Awake() {
+        coll = GetComponent<Collider2D>();
+    }
+
+
     private void OnTriggerExit2D(Collider2D collision) {
         if (!collision.CompareTag("Area"))
             return;
@@ -22,14 +30,18 @@ public class Reposition : MonoBehaviour{
             case "Ground":
                 if(diffX > diffY) {
                     transform.Translate(Vector3.right * dirX * 40);
+                    Debug.Log(Vector3.right + " " +  dirX + " " + "40" + this.name);
                 }
                 else if (diffX < diffY) {
                     transform.Translate(Vector3.up * dirY * 40);
+                    Debug.Log(Vector3.up + " " + dirY + " " + "40" + this.name);
                 }
                 break;
 
             case "Enemy":
-
+                if (coll.enabled) {
+                    transform.Translate(playerDir * 20 + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0));
+                }
                 break;
 
         }
